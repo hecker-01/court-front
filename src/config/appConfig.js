7a115153375ed34c37407c004a72ae17d65053fe
Hecker_01 <101524930+Hecker-01@users.noreply.github.com>
2026-03-12 08:53:45 +0100

    fix: correct API base URL by removing port number for proper connectivity

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -1,7 +1,7 @@
 const appConfig = {
   name: "King of the Court",
   // apiBaseUrl: "http://172.16.131.197:3000/api",
-  apiBaseUrl: "https://court-api.heckerdev.net:3000/api",
+  apiBaseUrl: "https://court-api.heckerdev.net/api",
 };
 
 export default appConfig;
